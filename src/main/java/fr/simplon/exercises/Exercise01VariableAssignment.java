dc2d3598685d7cde0ef794df752cec6b94ca0ff6
DEHAINAULT Quentin <quentin.dehainault@maif.fr>
2026-02-09 11:38:52 +0100

    feature: ajout de l'exercice 1

--- src/main/java/fr/simplon/exercises/Exercise01VariableAssignment.java
+++ src/main/java/fr/simplon/exercises/Exercise01VariableAssignment.java
@@ -6,28 +6,34 @@ package fr.simplon.exercises;
  * Objectif: Apprendre à assigner des valeurs à des variables
  */
 public class Exercise01VariableAssignment {
-    
+
     /**
      * Assigne la valeur 42 à une variable et la retourne
+     * 
      * @return la valeur 42
      */
     public int assignInteger() {
-        throw new UnsupportedOperationException();
+        int value = 42;
+        return value;
     }
-    
+
     /**
      * Assigne ton prénom à une variable et la retourne
+     * 
      * @return ton prénom
      */
     public String assignString() {
-        throw new UnsupportedOperationException();
+        String prenom = "Quentin";
+        return prenom;
     }
-    
+
     /**
      * Assigne la valeur true à une variable et la retourne
+     * 
      * @return la valeur true
      */
     public boolean assignBoolean() {
-        throw new UnsupportedOperationException();
+        boolean vrai = true;
+        return vrai;
     }
 }
