5e9a18ff2c98803ac5e2855b6670d74caa0427df
DEHAINAULT Quentin <quentin.dehainault@maif.fr>
2026-02-09 11:43:57 +0100

    feature: ajout de l'exercice 2

--- src/main/java/fr/simplon/exercises/Exercise02ArithmeticOperations.java
+++ src/main/java/fr/simplon/exercises/Exercise02ArithmeticOperations.java
@@ -14,7 +14,7 @@ public class Exercise02ArithmeticOperations {
      * @return la somme de a et b
      */
     public int add(int a, int b) {
-        throw new UnsupportedOperationException();
+        return a + b;
     }
     
     /**
@@ -24,7 +24,7 @@ public class Exercise02ArithmeticOperations {
      * @return la différence entre a et b
      */
     public int subtract(int a, int b) {
-        throw new UnsupportedOperationException();
+        return a - b;
 
     }
     
@@ -35,7 +35,7 @@ public class Exercise02ArithmeticOperations {
      * @return le produit de a et b
      */
     public int multiply(int a, int b) {
-        throw new UnsupportedOperationException();
+        return a * b;
 
     }
     
@@ -46,7 +46,7 @@ public class Exercise02ArithmeticOperations {
      * @return le quotient de a divisé par b
      */
     public int divide(int a, int b) {
-        throw new UnsupportedOperationException();
+        return a / b;
 
     }
     
@@ -57,7 +57,7 @@ public class Exercise02ArithmeticOperations {
      * @return le reste de a divisé par b
      */
     public int modulo(int a, int b) {
-        throw new UnsupportedOperationException();
+        return a % b;
 
     }
 }
