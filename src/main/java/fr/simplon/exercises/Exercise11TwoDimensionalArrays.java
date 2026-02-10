7b5d5b41d5e64c087665e31c5a6e1591af16304c
DEHAINAULT Quentin <quentin.dehainault@maif.fr>
2026-02-10 19:11:35 +0100

    feature: ajout de l'exercice 11

--- src/main/java/fr/simplon/exercises/Exercise11TwoDimensionalArrays.java
+++ src/main/java/fr/simplon/exercises/Exercise11TwoDimensionalArrays.java
@@ -9,53 +9,82 @@ public class Exercise11TwoDimensionalArrays {
 
     /**
      * Crée une matrice remplie de zéros
+     * 
      * @param rows nombre de lignes
      * @param cols nombre de colonnes
      * @return une matrice rows x cols remplie de 0
      */
     public int[][] createMatrix(int rows, int cols) {
-        throw new UnsupportedOperationException();
+        return new int[rows][cols];
 
     }
-    
+
     /**
      * Calcule la somme de tous les éléments d'une matrice
+     * 
      * @param matrix la matrice
      * @return la somme de tous les éléments
      */
     public int sumMatrix(int[][] matrix) {
-        throw new UnsupportedOperationException();
-
+        int sum = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                sum = sum + matrix[i][j];
+            }
+        }
+        return sum;
     }
-    
+
     /**
      * Trouve le maximum dans une matrice
+     * 
      * @param matrix la matrice
      * @return la valeur maximale
      */
     public int findMaxInMatrix(int[][] matrix) {
-        throw new UnsupportedOperationException();
-
+        int max = matrix[0][0];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] > max ){
+                    max = matrix[i][j];
+                }
+            }
+        }
+        return max;
     }
-    
+
     /**
      * Transpose une matrice (lignes ↔ colonnes)
+     * 
      * @param matrix la matrice à transposer
      * @return la matrice transposée
      */
     public int[][] transpose(int[][] matrix) {
-        throw new UnsupportedOperationException();
-
+        int[][] inverse = createMatrix(matrix[0].length,matrix.length);
+        for (int i = 0; i < matrix[0].length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                    inverse[i][j] = matrix[j][i];
+            }
+        }
+        return inverse;
     }
-    
+
     /**
      * Retourne la diagonale principale d'une matrice carrée
+     * 
      * @param matrix la matrice carrée
      * @return un tableau contenant les éléments de la diagonale
      */
     public int[] getDiagonal(int[][] matrix) {
-        throw new UnsupportedOperationException();
-
+        int[] diagonal = new int[matrix.length];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (i == j){
+                    diagonal[i] = matrix[i][j];
+                }
+            }
+        }
+        return diagonal;
     }
-    
+
 }
