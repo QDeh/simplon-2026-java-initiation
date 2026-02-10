915b402c265edcb1dd8849b0d357c3cad5e336e3
DEHAINAULT Quentin <quentin.dehainault@maif.fr>
2026-02-10 10:09:59 +0100

    feature: ajout de l'exercice 8

--- src/main/java/fr/simplon/exercises/Exercise08StringManipulation.java
+++ src/main/java/fr/simplon/exercises/Exercise08StringManipulation.java
@@ -47,15 +47,7 @@ public class Exercise08StringManipulation {
         int compteur = 0;
         str = str.toLowerCase();
         for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i) == 'a') {
-                compteur++;
-            } else if (str.charAt(i) == 'e') {
-                compteur++;
-            } else if (str.charAt(i) == 'i') {
-                compteur++;
-            } else if (str.charAt(i) == 'o') {
-                compteur++;
-            } else if (str.charAt(i) == 'u') {
+            if (str.charAt(i) == 'a' || str.charAt(i) == 'e' || str.charAt(i) == 'i' || str.charAt(i) == 'o' || str.charAt(i) == 'u') {
                 compteur++;
             }
         }
