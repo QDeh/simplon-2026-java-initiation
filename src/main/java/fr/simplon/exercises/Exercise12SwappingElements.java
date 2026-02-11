3ad7bbb69e382ef4b82f707e925c70663c82bb78
DEHAINAULT Quentin <quentin.dehainault@maif.fr>
2026-02-11 09:49:49 +0100

    feature: ajout de l'exercice 12

--- src/main/java/fr/simplon/exercises/Exercise12SwappingElements.java
+++ src/main/java/fr/simplon/exercises/Exercise12SwappingElements.java
@@ -6,91 +6,122 @@ package fr.simplon.exercises;
  * Objectif: Apprendre les opérations de base nécessaires pour le tri
  */
 public class Exercise12SwappingElements {
-    
+
+    Exercise06ArraysMaxMin exercise06ArraysMaxMin = new Exercise06ArraysMaxMin();
+
     /**
      * Échange deux éléments dans un tableau
+     * 
      * @param array le tableau
-     * @param i premier index
-     * @param j second index
+     * @param i     premier index
+     * @param j     second index
      */
     public int[] swap(int[] array, int i, int j) {
-        throw new UnsupportedOperationException();
-
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+        return array;
     }
-    
+
     /**
      * Décale un élément vers la gauche
-     * @param array le tableau
+     * 
+     * @param array     le tableau
      * @param fromIndex position source (plus grande)
-     * @param toIndex position destination (plus petite)
+     * @param toIndex   position destination (plus petite)
      * @return le tableau modifié
      */
     public int[] shiftLeft(int[] array, int fromIndex, int toIndex) {
-        throw new UnsupportedOperationException();
-
+        for (int i = fromIndex; i > toIndex; i--) {
+            swap(array, i, i - 1);
+        }
+        return array;
     }
-    
+
     /**
      * Décale un élément vers la droite
-     * @param array le tableau
+     * 
+     * @param array     le tableau
      * @param fromIndex position source (plus petite)
-     * @param toIndex position destination (plus grande)
+     * @param toIndex   position destination (plus grande)
      * @return le tableau modifié
      */
     public int[] shiftRight(int[] array, int fromIndex, int toIndex) {
-        throw new UnsupportedOperationException();
-
+        for (int i = fromIndex; i < toIndex; i++) {
+            swap(array, i, i + 1);
+        }
+        return array;
     }
-    
+
     /**
      * Place le plus petit élément au début du tableau
+     * 
      * @param array le tableau
      * @return le tableau modifié
      */
     public int[] moveSmallestToFront(int[] array) {
-        throw new UnsupportedOperationException();
-
+        int indexMin = exercise06ArraysMaxMin.findMinIndex(array);
+        shiftLeft(array, indexMin, 0);
+        return array;
     }
-    
+
     /**
      * Place le plus grand élément à la fin du tableau
+     * 
      * @param array le tableau
      * @return le tableau modifié
      */
     public int[] moveLargestToEnd(int[] array) {
-        throw new UnsupportedOperationException();
-
+        int indexMax = exercise06ArraysMaxMin.findMaxIndex(array);
+        shiftRight(array, indexMax, array.length - 1);
+        return array;
     }
-    
+
     /**
      * Vérifie si deux éléments adjacents sont dans le bon ordre
+     * 
      * @param array le tableau
-     * @param i l'index du premier élément
+     * @param i     l'index du premier élément
      * @return true si array[i] <= array[i+1], false sinon
      */
     public boolean isInOrder(int[] array, int i) {
-        throw new UnsupportedOperationException();
+        if (array[i] <= array[i + 1]) {
+            return true;
+        }
+        return false;
 
     }
-    
+
     /**
      * Échange tous les éléments adjacents qui ne sont pas dans le bon ordre
      * (une seule passe)
+     * 
      * @param array le tableau
      * @return le tableau après une passe
      */
     public int[] bubblePass(int[] array) {
-        throw new UnsupportedOperationException();
-
+        for (int i = 0; i < array.length - 1; i++) {
+            if (!isInOrder(array, i)) {
+                swap(array, i, i + 1);
+                i++;
+            }
+        }
+        return array;
     }
-    
+
     /**
      * Compte le nombre de paires d'éléments adjacents mal ordonnées
+     * 
      * @param array le tableau
      * @return le nombre de paires où array[i] > array[i+1]
      */
     public int countInversions(int[] array) {
-        throw new UnsupportedOperationException();
-
+        int compt = 0;
+        for (int i = 0; i < array.length - 1; i++) {
+            if (!isInOrder(array, i)) {
+                compt++;
+            }
+        }
+        return compt;
     }
 }
