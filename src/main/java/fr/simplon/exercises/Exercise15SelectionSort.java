298d6deb2ca85156878968a120e8da0c95d324ca
DEHAINAULT Quentin <quentin.dehainault@maif.fr>
2026-02-11 16:39:02 +0100

    feature: ajout de l'exercice 15

--- src/main/java/fr/simplon/exercises/Exercise15SelectionSort.java
+++ src/main/java/fr/simplon/exercises/Exercise15SelectionSort.java
@@ -3,48 +3,99 @@ package fr.simplon.exercises;
 /**
  * Exercice 15: Tri par sélection (Selection Sort)
  * 
- * Objectif: Apprendre un algorithme de tri efficace basé sur la sélection du minimum
+ * Objectif: Apprendre un algorithme de tri efficace basé sur la sélection du
+ * minimum
  */
 public class Exercise15SelectionSort {
-    
+
+    Exercise12SwappingElements exercise12 = new Exercise12SwappingElements();
+
     /**
      * Trie un tableau en utilisant l'algorithme de tri par sélection
+     * 
      * @param array le tableau à trier
      * @return le tableau trié
      */
     public int[] selectionSort(int[] array) {
-        throw new UnsupportedOperationException();
+        int minIndex = 0;
+        for (int i = 0; i < array.length; i++) {
+            minIndex = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < array[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            exercise12.swap(array, minIndex, i);
+        }
+        return array;
 
     }
-    
+
     /**
      * Trie un tableau en ordre décroissant avec le tri par sélection
+     * 
      * @param array le tableau à trier
      * @return le tableau trié en ordre décroissant
      */
     public int[] selectionSortDescending(int[] array) {
-        throw new UnsupportedOperationException();
+        int maxIndex = 0;
+        for (int i = 0; i < array.length; i++) {
+            maxIndex = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] > array[maxIndex]) {
+                    maxIndex = j;
+                }
+            }
+            exercise12.swap(array, maxIndex, i);
+        }
+        return array;
 
     }
-    
+
     /**
      * Trouve les k plus petits éléments d'un tableau (non triés entre eux)
+     * 
      * @param array le tableau
-     * @param k le nombre d'éléments à trouver
+     * @param k     le nombre d'éléments à trouver
      * @return un tableau contenant les k plus petits éléments
      */
     public int[] findKSmallest(int[] array, int k) {
-        throw new UnsupportedOperationException();
+        int[] kSmallest = new int[k];
+        int minIndex = 0;
+        for (int i = 0; i < k; i++) {
+            minIndex = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < array[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            kSmallest[i] = array[minIndex];
+            exercise12.swap(array, minIndex, i);
+        }
+        return kSmallest;
 
     }
-    
+
     /**
      * Compte le nombre de comparaisons effectuées pendant le tri par sélection
+     * 
      * @param array le tableau à trier
      * @return le nombre de comparaisons
      */
     public int countComparisons(int[] array) {
-        throw new UnsupportedOperationException();
+        int minIndex = 0;
+        int compt = 0;
+        for (int i = 0; i < array.length; i++) {
+            minIndex = i;
+            for (int j = i; j < array.length; j++) {
+                if (array[j] < array[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            exercise12.swap(array, minIndex, i);
+            compt++;
+        }
+        return compt;
 
     }
 }
