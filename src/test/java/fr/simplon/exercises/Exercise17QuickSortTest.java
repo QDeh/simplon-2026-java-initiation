f7372908efdfebcbff63b359425381a098f650bd
DEHAINAULT Quentin <quentin.dehainault@maif.fr>
2026-02-12 09:42:32 +0100

    feature: ajout de l'énoncé de l'exercice 17

--- src/test/java/fr/simplon/exercises/Exercise17QuickSortTest.java
+++ src/test/java/fr/simplon/exercises/Exercise17QuickSortTest.java
@@ -12,36 +12,18 @@ class Exercise17QuickSortTest {
     
     @Test
     void testQuickSort() {
-        assertArrayEquals(new int[]{1, 2, 3, 4, 5}, exercise.quickSort(new int[]{5, 2, 4, 1, 3}));
-        assertArrayEquals(new int[]{1, 2, 3, 4, 5}, exercise.quickSort(new int[]{1, 2, 3, 4, 5}));
-        assertArrayEquals(new int[]{1, 2, 3, 4, 5}, exercise.quickSort(new int[]{5, 4, 3, 2, 1}));
-        assertArrayEquals(new int[]{1, 2, 3, 6, 7}, exercise.quickSort(new int[]{7, 3, 1, 6, 2}));
-        assertArrayEquals(new int[]{10, 20, 30, 40}, exercise.quickSort(new int[]{40, 10, 30, 20}));
+        assertArrayEquals(new int[]{1, 2, 3, 4, 5}, exercise.quickSort(new int[]{5, 2, 4, 1, 3},0,4));
+        assertArrayEquals(new int[]{1, 2, 3, 4, 5}, exercise.quickSort(new int[]{1, 2, 3, 4, 5},0,4));
+        assertArrayEquals(new int[]{1, 2, 3, 4, 5}, exercise.quickSort(new int[]{5, 4, 3, 2, 1},0,4));
+        assertArrayEquals(new int[]{1, 2, 3, 6, 7}, exercise.quickSort(new int[]{7, 3, 1, 6, 2},0,4));
+        assertArrayEquals(new int[]{10, 20, 30, 40}, exercise.quickSort(new int[]{40, 10, 30, 20},0,3));
     }
     
     @Test
     void testQuickSortDescending() {
-        assertArrayEquals(new int[]{5, 4, 3, 2, 1}, exercise.quickSortDescending(new int[]{5, 2, 4, 1, 3}));
-        assertArrayEquals(new int[]{5, 4, 3, 2, 1}, exercise.quickSortDescending(new int[]{1, 2, 3, 4, 5}));
-        assertArrayEquals(new int[]{7, 6, 3, 2, 1}, exercise.quickSortDescending(new int[]{7, 3, 1, 6, 2}));
-        assertArrayEquals(new int[]{40, 30, 20, 10}, exercise.quickSortDescending(new int[]{10, 40, 20, 30}));
-    }
-    
-    @Test
-    void testCountShifts() {
-        assertEquals(0, exercise.countShifts(new int[]{1, 2, 3, 4, 5})); // Déjà trié, aucun décalage
-        assertTrue(exercise.countShifts(new int[]{5, 4, 3, 2, 1}) > 0); // Plusieurs décalages nécessaires
-        assertTrue(exercise.countShifts(new int[]{2, 1, 3}) > 0); // Au moins un décalage
-        assertTrue(exercise.countShifts(new int[]{3, 1, 2}) > 0);
-        assertEquals(0, exercise.countShifts(new int[]{10, 20, 30}));
-    }
-    
-    @Test
-    void testInsertIntoSorted() {
-        assertArrayEquals(new int[]{1, 2, 3, 4, 5}, exercise.insertIntoSorted(new int[]{1, 2, 4, 5}, 3));
-        assertArrayEquals(new int[]{0, 1, 2, 3}, exercise.insertIntoSorted(new int[]{1, 2, 3}, 0));
-        assertArrayEquals(new int[]{1, 2, 3, 10}, exercise.insertIntoSorted(new int[]{1, 2, 3}, 10));
-        assertArrayEquals(new int[]{5, 10, 15, 20}, exercise.insertIntoSorted(new int[]{5, 10, 20}, 15));
-        assertArrayEquals(new int[]{1, 7, 10, 20}, exercise.insertIntoSorted(new int[]{1, 10, 20}, 7));
+        assertArrayEquals(new int[]{5, 4, 3, 2, 1}, exercise.quickSortDescending(new int[]{5, 2, 4, 1, 3},0,4));
+        assertArrayEquals(new int[]{5, 4, 3, 2, 1}, exercise.quickSortDescending(new int[]{1, 2, 3, 4, 5},0,4));
+        assertArrayEquals(new int[]{7, 6, 3, 2, 1}, exercise.quickSortDescending(new int[]{7, 3, 1, 6, 2},0,4));
+        assertArrayEquals(new int[]{40, 30, 20, 10}, exercise.quickSortDescending(new int[]{40, 10, 30, 20},0,3));
     }
 }
